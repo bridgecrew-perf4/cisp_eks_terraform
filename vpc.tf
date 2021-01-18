dffb087cd77d30b099e0c83f4600160ed65b7f9a
Júlio Ballot <julio.ballot@4linux.com.br>
2021-01-18 14:07:28 -0300

    Adicionando configurações de VPC e Subnet via vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,53 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
+
+resource "aws_subnet" "public" {
+  count = 3
+  vpc_id = data.aws_vpc.selected.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  
+
+  tags = {
+    format("kubernetes.io/cluster/%s",var.eks_cluster_name) = "shared"
+    "kubernetes.io/cluster/elb" = "1"
+    Name = format("pubsub-%s-%s",var.eks_cluster_name,count.index)
+  }
+}
+
+resource "aws_subnet" "private" {
+  count= 3
+  vpc_id = data.aws_vpc.selected.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block = var.private_subnets[count.index]
+  
+
+  tags = {
+    format("kubernetes.io/cluster/%s",var.eks_cluster_name) = "shared"
+    "kubernetes.io/cluster/internal-elb" = "1"
+    Name = format("privsub-%s-%s",var.eks_cluster_name,count.index)
+  }
+}
+
+
+
+data "aws_internet_gateway" "default" {
+  filter {
+    name = "attachment.vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
+ 
+resource "aws_route" "main" {
+  route_table_id         = data.aws_vpc.selected.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = data.aws_internet_gateway.default.id
+}
+
