de89bffe9bd26b24ee1a4820b5ebcae0b8384163
Júlio Ballot <julio.ballot@4linux.com.br>
2021-01-20 15:13:15 -0300

    Adicionando output para o eks e rds

---
+++ output.tf
@@ -0,0 +1,12 @@
+
+output "endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "rds_mariadb_endpoint" {
+  value = aws_db_instance.default.endpoint
+}
