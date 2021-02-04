847bd095dfda197a718c2619c05551851a84e1d1
Júlio Ballot <julio.ballot@4linux.com.br>
2021-02-04 13:46:32 -0300

    Atualizando configuração de rede do Cluster EKS
    Nessa atualização estamos adicionando um rede privada para reestruturar
    o cluster como sendo Publico e Privado.

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,12 +1,16 @@
+##
+## AWS EKS Cluster
+##
 resource "aws_eks_cluster" "main" {
-  name = var.eks_cluster_name
+  name     = var.eks_cluster_name
   role_arn = aws_iam_role.main.arn
-  version = var.eks_k8s_version 
+  version  = var.eks_k8s_version
 
   vpc_config {
-    subnet_ids = aws_subnet.public[*].id
+    subnet_ids              = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
     endpoint_private_access = true
-    endpoint_public_access = true
+    endpoint_public_access  = true
+    security_group_ids      = [aws_security_group.allow_output_from_private.id]
   }
 
   depends_on = [
@@ -15,7 +19,7 @@ resource "aws_eks_cluster" "main" {
   ]
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
   }
 
 }
