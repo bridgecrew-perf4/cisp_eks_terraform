61fee5a793aced5af3bf9977007e8200695b7e6a
Júlio Ballot <julio.ballot@4linux.com.br>
2021-02-04 14:08:26 -0300

    Atualizando formatação do varaibles.tf com terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
-  default = "sa-east-1"
+  default     = "sa-east-1"
   description = "AWS São Paulo Region"
 }
 
 variable "aws_bucket_name" {
-  default = "cisp-tf-state"
+  default     = "cisp-tf-state"
   description = "AWS S3 Bucket for Terraform State"
 }
 
 variable "aws_dynamodb_table_name" {
-  default = "cips-tf-lock"
+  default     = "cips-tf-lock"
   description = "AWS DynamoDB table for Terraform Lock"
 }
 
@@ -17,27 +17,27 @@ variable "aws_dynamodb_table_name" {
 ## EKS Variables
 ##
 variable "eks_k8s_version" {
-  default = "1.18"
+  default     = "1.18"
   description = "AWS EKS's Kubernetes version"
 }
 
 variable "eks_cluster_name" {
-  default = "cisp_eks"
+  default     = "cisp_eks"
   description = "AWS EKS Cluster Name"
 }
 #
 variable "eks_node_group_name" {
-  default = "cisp_eks_node_group"
+  default     = "cisp_eks_node_group"
   description = "AWS EKS Node Group Name"
 }
 
 variable "eks_instance_types" {
-  default = ["t3.medium"]
+  default     = ["t3.medium"]
   description = "AWS EC2 Instance Type to be used on EKS cluster"
 }
 
 variable "eks_instance_disk_size" {
-  default = 40
+  default     = 40
   description = "AWS EC2 Instance Disk Size in GiB"
 }
 
@@ -46,63 +46,63 @@ variable "eks_instance_disk_size" {
 #
 
 variable "rds_storage" {
-  default = "20"
+  default     = "20"
   description = "RDS minimum storage size"
 }
 variable "rds_max_storage" {
-  default = "10000"
+  default     = "10000"
   description = "RDS minimum storage size"
 }
 
 variable "rds_type" {
-  default = "gp2"
+  default     = "gp2"
   description = "RDS type"
 }
 
 variable "rds_name" {
-  default = "cisp_rds_mariadb"
+  default     = "cisp_rds_mariadb"
   description = "RDS Name"
 }
 
 variable "rds_identifier" {
-  default = "cisp-rds-mariadb"
+  default     = "cisp-rds-mariadb"
   description = "RDS Identifier"
 }
 
 variable "rds_instance_class" {
-  default = "db.m5.large"
+  default     = "db.m5.large"
   description = "RDS Instance Class. The m5.large is the latest model (01/2021) with 2vCPUS and 8GiB RAM"
 }
 
 variable "rds_engine" {
-  default = "mariadb"
+  default     = "mariadb"
   description = "RDS Engine used"
 }
 
 variable "rds_engine_version" {
-  default = "10.5"
+  default     = "10.5"
   description = "RDS Engine version to be used"
 }
 
 variable "rds_username" {
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "RDS access user."
 }
 
 variable "rds_pass" {
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "RDS password."
 }
 
 variable "rds_backup_retention_period" {
-  default = 30
+  default     = 30
   description = "RDS Database backup retention in days"
 }
 
 variable "rds_backup_window" {
-  default = "00:00-01:00"
+  default     = "00:00-01:00"
   description = "RDS Backup window"
 }
 
@@ -110,33 +110,33 @@ variable "rds_backup_window" {
 ## VPC Variables
 ##
 variable "vpc_id" {
-  default = "vpc-4dbce728"
+  default     = "vpc-4dbce728"
   description = "AWS VPC default id value for CISP account"
 }
 #
 variable "public_subnets" {
-  default = ["172.31.200.0/24","172.31.201.0/24","172.31.202.0/24"]
+  default     = ["172.31.200.0/24", "172.31.201.0/24", "172.31.202.0/24"]
   description = "AWS EKS public networks"
 }
 
 variable "private_subnets" {
-  default = ["172.31.203.0/24","172.31.204.0/24","172.31.205.0/24"]
+  default     = ["172.31.203.0/24", "172.31.204.0/24", "172.31.205.0/24"]
   description = "AWS EKS private networks"
 }
 
 
 ## ECR Variables
 variable "repo-name" {
-  default = ["cisp-configserver", "cisp-eureka", "cisp-auth", "cisp-zuul", "cisp-segmento", "cisp-frontend" ]
+  default     = ["cisp-configserver", "cisp-eureka", "cisp-auth", "cisp-zuul", "cisp-segmento", "cisp-frontend"]
   description = "The repos that will be created."
 }
 
 variable "image-mutability" {
-  default = "MUTABLE"
+  default     = "MUTABLE"
   description = "If the tags can be changed. Options can be 'MUTABLE' or 'IMMUTABLE'"
 }
 
 variable "image-scan" {
- default = "true"
- description = "If the images should be scanned when pushed" 
+  default     = "true"
+  description = "If the images should be scanned when pushed"
 }
