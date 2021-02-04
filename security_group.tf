075fd33e91321683d3879432aa6dc3c74d6adb5c
Júlio Ballot <julio.ballot@4linux.com.br>
2021-02-04 14:07:40 -0300

    Adicionando security group para saída das maquinas da rede privada via NAT Gateway

--- security_group.tf
+++ security_group.tf
@@ -1,24 +1,37 @@
 
 resource "aws_security_group" "allow_mariadb" {
 
-  name = "mariadb_sg"
+  name        = "mariadb_sg"
   description = "MariaDB Security Group"
-  vpc_id = data.aws_vpc.selected.id
+  vpc_id      = data.aws_vpc.selected.id
 
   ingress {
     description = "MariaDB Access"
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
     # cidr_blocks é a origem da conexão
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "Allow output to all addresses"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "allow_output_from_private" {
+  name        = "Allow Egress from EKS node"
+  description = "Allow EKS nodes to reach the internet"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
