bb7ac48060853129d9b21cc30eb089dae25d28c1
Júlio Ballot <julio.ballot@4linux.com.br>
2021-02-04 14:04:43 -0300

    Atualizando RDS para operar na rede privada
    Rodando o RDS em rede privada garante maior segurança para a plataforma.

--- rds.tf
+++ rds.tf
@@ -1,39 +1,48 @@
+##
+## AWS RDS db Parameters
+##
 #resource "aws_db_parameter_group" "default" {
 #  name = "mariadb"
 #  family = "mariadb10.5"
 #}
 
+##
+## AWS RDS Subnet definition
+##
 resource "aws_db_subnet_group" "default" {
-  name = "main"
+  name       = "main"
   subnet_ids = aws_subnet.private[*].id
 }
 
+##
+## AWS RDS DB Instance
+##
 resource "aws_db_instance" "default" {
-  identifier = var.rds_identifier
-  storage_type = var.rds_type
-  engine = var.rds_engine
+  identifier     = var.rds_identifier
+  storage_type   = var.rds_type
+  engine         = var.rds_engine
   engine_version = var.rds_engine_version
   instance_class = var.rds_instance_class
-  name = var.rds_name
-  username = var.rds_username
-  password = var.rds_pass
-#  parameter_group_name = aws_db_parameter_group.default.name
+  name           = var.rds_name
+  username       = var.rds_username
+  password       = var.rds_pass
+  #  parameter_group_name = aws_db_parameter_group.default.name
   db_subnet_group_name = aws_db_subnet_group.default.name
- 
+
   ## Configuração de MultiAZ
   multi_az = true
 
-  ## Security Groups
+  ## Security Groups e acesso público (não funciona tendo a rede como privada)
   vpc_security_group_ids = list(aws_security_group.allow_mariadb.id)
-  publicly_accessible = true
- 
+  publicly_accessible    = false
+
   ## AWS RDS Autoscaling
-  allocated_storage = var.rds_storage
+  allocated_storage     = var.rds_storage
   max_allocated_storage = var.rds_max_storage
 
   ## Configurações de Backup
   backup_retention_period = var.rds_backup_retention_period
-  backup_window = var.rds_backup_window
+  backup_window           = var.rds_backup_window
 
   ## Não realizar o snapshot ao deletar a instância
   skip_final_snapshot = true
