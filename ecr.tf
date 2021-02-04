9bd986b5e01308fe3f58650e6ec4dfa0c089189e
Júlio Ballot <julio.ballot@4linux.com.br>
2021-02-04 13:45:48 -0300

    Atualizando ecr policy
    Nova policy irá permitir imagens sem tags por até 5 dias que posteriormente serão expirados.

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,8 @@
+##
+## AWS ECR
+##
 resource "aws_ecr_repository" "main" {
-  for_each = toset(var.repo-name)
+  for_each             = toset(var.repo-name)
   name                 = each.key
   image_tag_mutability = var.image-mutability
 
@@ -8,3 +11,31 @@ resource "aws_ecr_repository" "main" {
   }
 }
 
+##
+## AWS ECR Lifecycle policy
+## Irá remover as imagens sem tags do ECR
+##
+
+resource "aws_ecr_lifecycle_policy" "cleanup" {
+  for_each   = toset(var.repo-name)
+  repository = each.key
+  policy     = <<EOF
+  {
+    "rules": [
+       {
+         "rulePriority": 1,
+         "description": "Remove imagens sem tags",
+         "selection": {
+           "tagStatus": "untagged",
+           "countType": "sinceImagePushed",
+           "countUnit": "days",
+           "countNumber": 5
+         },
+         "action": {
+           "type": "expire"
+         }
+       }
+    ]
+  }
+  EOF
+}
