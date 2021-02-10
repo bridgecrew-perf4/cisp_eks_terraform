9b21fc70cfe4aafeeb611a8054a05ea69db39253
Júlio Ballot <julio.ballot@4linux.com.br>
2021-02-10 17:42:58 -0300

    Adicionando diretório com scripts para customização do EKS
    Devido a necessidade de instalar o Metric Server e Auto Scaler no EKS, precisei adicionar um script em shell para realizar a aplicação dos manifestos correspondentes. O script está com nome k8s_config.sh dentro do diretório scripts.

---
+++ scripts/k8s_config.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+
+
+AWS_REGION=$1
+EKS_CLUSTER=$2
+IAM_CLUSTER_ROLE=$3
+
+check_aws_credentials(){
+  if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo "[ERROR] AWS_ACCESS_KEY_ID not defined, please check if your AWS credentials variables ware defined."
+    exit 1
+  fi
+
+  if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "[ERROR] AWS_SECRET_ACCESS_KEY not defined, please check if your AWS credentials variables ware defined."
+    exit 1
+  fi
+  
+}
+
+update_eks_kubeconfig(){
+
+  aws eks --region $AWS_REGION update-kubeconfig --name $EKS_CLUSTER
+
+  if [ $? != 0 ]; then
+    echo "[ERROR] Something whent wrong when trying to update your kubeconfig"
+    exit 1
+  fi
+
+}
+
+apply_k8s_metric_server(){
+
+  kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+  if [ $? != 0 ]; then
+    echo "[ERROR] Something whent wrong when trying to create the Metric Server"
+    exit 1
+  fi
+  
+}
+
+
+apply_eks_cluster_autoscaler(){
+
+  sed -i "s/<YOUR CLUSTER NAME>/$EKS_CLUSTER/g" manifests/cluster-autoscaler-autodiscover.yaml
+
+  kubectl apply -f manifests/cluster-autoscaler-autodiscover.yaml
+
+  kubectl annotate --overwrite=true serviceaccount cluster-autoscaler -n kube-system eks.amazonaws.com/role-arn=${IAM_CLUSTER_ROLE}
+
+  kubectl patch deployment cluster-autoscaler \
+  -n kube-system \
+  -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}'
+
+
+  kubectl set image deployment cluster-autoscaler \
+  -n kube-system \
+  cluster-autoscaler=k8s.gcr.io/autoscaling/cluster-autoscaler:v1.18
+
+
+}
+
+
+
+check_aws_credentials
+update_eks_kubeconfig
+apply_k8s_metric_server
+apply_eks_cluster_autoscaler
+
+
