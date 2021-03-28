a3f4d0b6151a6ffb8a55755733da6f7715056c5c
Júlio Ballot <julio.ballot@4linux.com.br>
2021-03-28 08:51:27 -0300

    Changing IAM TF resource name from main2 to eks_node_group_role

--- iam.tf
+++ iam.tf
@@ -34,8 +34,8 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 ##
 ## Allow AWS EC2 to call other resources/services in your behalf
 ##
-resource "aws_iam_role" "main2" {
-  name = "poc_eks_node_group_role"
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks_node_group_role"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -51,17 +51,17 @@ resource "aws_iam_role" "main2" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.main2.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.main2.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.main2.name
+  role       = aws_iam_role.eks_node_group_role.name
 }
 
 
@@ -147,7 +147,92 @@ resource "aws_iam_policy" "AmazonEKStoRDSAccess" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKStoRDSAccess" {
   policy_arn = aws_iam_policy.AmazonEKStoRDSAccess.arn
-  role = aws_iam_role.main2.name
+  role = aws_iam_role.eks_node_group_role.name
 }
 
 
+
+
+##
+## EKS Role for Route53
+##
+#resource "aws_iam_role" "AllowExternalDNSUpdates" {
+#  name = "AllowExternalDNSUpdates"
+#
+#  #assume_role_policy = jsonencode({
+#  #  "Version" : "2012-10-17",
+#  #  "Statement" : [
+#  #    {
+#  #      "Effect" : "Allow",
+#  #      "Principal" : {
+#  #        "Federated" : format("arn:aws:iam::707413322123:oidc-provider/%s", replace(aws_eks_cluster.main.identity[0].oidc[0].issuer,"https://",""))
+#  #      },
+#  #      "Action" : "sts:AssumeRoleWithWebIdentity",
+#  #      "Condition" : {
+#  #        "StringEquals" : {
+#  #          format("%s:sub",replace(aws_eks_cluster.main.identity[0].oidc[0].issuer,"https://","")) : "system:serviceaccount:kube-system:external-dns"
+#  #        }
+#  #      }
+#  #    }
+#  #  ]
+#  #})
+#  assume_role_policy = jsonencode({
+#    Statement = [{
+#      Action = "sts:AssumeRole"
+#      Effect = "Allow"
+#      Principal = {
+#        Service = "ec2.amazonaws.com"
+#      }
+#    },
+#    {
+#      Action = "sts:AssumeRole"
+#      Effect = "Allow"
+#      Principal = {
+#        Identifiers = format("%s",aws_iam_role.eks_node_group_role.arn)
+#      }
+#    }]
+#    Version = "2012-10-17"
+#  })
+#
+#}
+#
+#
+###
+### EKS Policy for Route53
+###
+#resource "aws_iam_policy" "AllowExternalDNSUpdates" {
+#  name = "AllowExternalDNSUpdates"
+#  description = "Policy that allows EKS to update Route53 records"
+#
+#  policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Effect": "Allow",
+#      "Action": [
+#        "route53:ChangeResourceRecordSets"
+#      ],
+#      "Resource": [
+#        "arn:aws:route53:::hostedzone/*"
+#      ]
+#    },
+#    {
+#      "Effect": "Allow",
+#      "Action": [
+#        "route53:ListHostedZones",
+#        "route53:ListResourceRecordSets"
+#      ],
+#      "Resource": [
+#        "*"
+#      ]
+#    }
+#  ]
+#}
+#  EOF
+#}
+#
+#resource "aws_iam_role_policy_attachment" "AllowExternalDNSUpdates" {
+#  policy_arn = aws_iam_policy.AllowExternalDNSUpdates.arn
+#  role = aws_iam_role.AllowExternalDNSUpdates.name
+#}
