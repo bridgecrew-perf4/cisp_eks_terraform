b9651ceb16e4898e56b13e59e838179ddabb88cc
Júlio Ballot <julio.ballot@4linux.com.br>
2021-03-28 08:59:58 -0300

    Updating output.tf with autoscaler_role, eks_lb_arn and external-dns-arn

--- output.tf
+++ output.tf
@@ -3,9 +3,9 @@ output "endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
 
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
-}
+#output "kubeconfig-certificate-authority-data" {
+#  value = aws_eks_cluster.main.certificate_authority[0].data
+#}
 
 output "rds_mariadb_endpoint" {
   value = aws_db_instance.apps.endpoint
@@ -19,3 +19,17 @@ data "aws_caller_identity" "current" {}
 output "account_id"{
   value = data.aws_caller_identity.current.account_id
 }
+
+
+output "autoscaler_role" {
+ value = aws_iam_role.AmazonEKSClusterAutoscalerRole.arn
+}
+
+output "eks_lb_arn" {
+  value = aws_iam_role.eks_lb.arn
+}
+
+
+#output "external-dns-arn" {
+#  value = aws_iam_role.AllowExternalDNSUpdates.arn
+#}
